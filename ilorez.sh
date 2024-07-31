ed830f77d64e1f6f86ed2c6ed347c9ba23d0fbf3
ilorez <zobirnajdaoui@gmail.com>
2024-07-31 23:31:13 +0100

    write my username with different tft fonts

---
+++ ilorez.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+name="ilorez"
+
+echo "standard"
+echo "======================"
+figlet $name
+echo "======================"
+
+echo "slant"
+echo "======================"
+figlet -f slant $name
+echo "======================"
+
+echo "binnary"
+echo "======================"
+figlet -f binary $name
+echo "======================"
+
+echo "alligator2"
+echo "======================"
+figlet -f alligator2 $name
+echo "======================"
+
+echo "banner"
+echo "======================"
+figlet -f banner $name
+echo "======================"
+
+echo "cosmic"
+echo "======================"
+figlet -f cosmic $name
+echo "======================"
+
+echo "cosmike"
+echo "======================"
+figlet -f cosmike $name
+echo "======================"
+
+echo "doh"
+echo "======================"
+figlet -f doh $name
+echo "======================"
+
+echo "graffiti"
+echo "======================"
+figlet -f graffiti $name
+echo "======================"
+
+echo "isometric1"
+echo "======================"
+figlet -f isometric1 $name
+echo "======================"
+
+echo "isometric2"
+echo "======================"
+figlet -f isometric2 $name
+echo "======================"
+
+echo "isometric3"
+echo "======================"
+figlet -f isometric3 $name
+echo "======================"
+
+echo "isometric4"
+echo "======================"
+figlet -f isometric4 $name
+echo "======================"
+
+echo "larry3d"
+echo "======================"
+figlet -f larry3d $name
+echo "======================"
+
+echo "letters"
+echo "======================"
+figlet -f letters $name
+echo "======================"
+
+echo "marquee"
+echo "======================"
+figlet -f marquee $name
+echo "======================"
+
+echo "peaks"
+echo "======================"
+figlet -f peaks $name
+echo "======================"
+
+echo "smisome1"
+echo "======================"
+figlet -f smisome1 $name
+echo "======================"
+
+echo "speed"
+echo "======================"
+figlet -f speed $name
+echo "======================"
+
+echo "weird"
+echo "======================"
+figlet -f weird $name
+echo "======================"
