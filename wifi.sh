9a005271bfd0d2895a49bc2662d3562eb465df3c
ilorez <zobirnajdaoui@gmail.com>
2024-08-08 09:30:31 +0100

    connect to wifi script

---
+++ wifi.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Scan for available WiFi networks
+networks=$(nmcli -t -f SSID dev wifi list | grep -v '^$')
+
+# Display available networks with numbers
+echo "Available WiFi networks:"
+IFS=$'\n' read -r -d '' -a network_array <<<"$networks"
+for i in "${!network_array[@]}"; do
+  echo "$((i + 1)). ${network_array[$i]}"
+done
+
+# Prompt the user to select a network
+read -p "Enter the number of the WiFi network you want to connect to: " network_number
+
+# Check if the input is a valid number
+if ! [[ "$network_number" =~ ^[0-9]+$ ]] || [ "$network_number" -lt 1 ] || [ "$network_number" -gt "${#network_array[@]}" ]; then
+  echo "Invalid number. Exiting."
+  exit 1
+fi
+
+# Get the selected network's SSID
+selected_network="${network_array[$((network_number - 1))]}"
+
+# Prompt the user for the WiFi password
+read -sp "Enter the password for '$selected_network': " wifi_password
+echo
+
+# Connect to the selected WiFi network
+nmcli dev wifi connect "$selected_network" password "$wifi_password"
+
+# Check if the connection was successful
+if [ $? -eq 0 ]; then
+  echo "Connected to '$selected_network' successfully."
+else
+  echo "Failed to connect to '$selected_network'."
+fi
